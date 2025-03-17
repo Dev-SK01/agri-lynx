c1a5d3504eac8ad28f29030bbae0a39093320956
ajith <ajith@localhost>
2025-03-17 20:24:06 +0530

    folder created for application users

--- agri-lynx/src/App.jsx
+++ agri-lynx/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import LocalMarketOwnerRegistration from './owners/LocalMarketOwnerRegistration'
 function App() {
 
 
   return (
     <>
+    <LocalMarketOwnerRegistration />
     </>
   )
 }
---
+++ agri-lynx/src/owners/LocalMarketOwnerRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LocalMarketOwnerRegistration = () => {
+  return (
+    <div>
+      <h1 className='font-arial bg-(--secondary)'>Registration</h1>
+    </div>
+  )
+}
+
+export default LocalMarketOwnerRegistration
